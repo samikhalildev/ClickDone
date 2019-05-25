df548eaffd030ce2af4e9581ad6241e0ff5d88ab
samikhalildev <sami.khalil.dev@gmail.com>
2019-05-26 00:29:47 +1000

    Fix date bug

--- src/Helpers/getCurrentDate.js
+++ src/Helpers/getCurrentDate.js
@@ -1,13 +1,10 @@
-var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-
 const getCurrentDate = () => {
     let date = new Date()
 
     let year = date.getFullYear();
-    var month = months[date.getMonth()].substring(0, 3);
+    var month = date.toDateString().substring(4, 7);
+    let weekDay = date.toDateString().substring(0, 3);
     let day = date.getDate();
-    let weekDay = days[date.getDay()].substring(0, 3);
     
     if (month.length < 2) month = '0' + month;
     if (day.length < 2) day = '0' + day;
@@ -25,7 +22,7 @@ const getCurrentDate = () => {
     if ((AMPM === 'am' && hours >= 6 && hours <= 11) || (AMPM === 'pm' && hours === 12 || hours <= 5))
         emoji = `🌞`;
 
-    else if ((AMPM === 'pm' && hours >= 6 && hours <= 11) || (AMPM === 'am' && hours >= 0 && hours <= 5))
+    else if ((AMPM === 'pm' && hours >= 6 && hours <= 11) || (AMPM === 'am' && hours === 12 || hours <= 5))
         emoji = `🌚`
     
     return `${weekDay}, ${day} ${month} ${year} at ${hours}:${minutes} ${AMPM} ${emoji}`;
